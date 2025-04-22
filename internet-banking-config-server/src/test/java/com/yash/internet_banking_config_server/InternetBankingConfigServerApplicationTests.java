288e54c295e198a8ae74705e531aad0fbb4531c2
Yash <yp1679149@gmail.com>
2025-04-22 18:20:51 -0400

    add_Configuration_server(use Git)

---
+++ internet-banking-config-server/src/test/java/com/yash/internet_banking_config_server/InternetBankingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.internet_banking_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
